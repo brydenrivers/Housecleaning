b8d7886e87ad4584197b36f08a089c26f4074aa8
Luis <luiggiblade@gmail.com>
2021-02-08 02:26:15 -0600

    queries and js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const navIcon = document.querySelector('.mobile-nav-icon');
 const navMenu = document.querySelector('.nav-pages');
 const topBar = document.querySelector('.top-nav');
+const logo = document.querySelector('.logo');
 navIcon.addEventListener('click', () =>{
     navMenu.classList.toggle('mobile-options');
     topBar.classList.toggle('mobile-options');
+    logo.classList.toggle('mobile-options');
 });
+
 // navIcon.addEventListener('touchstart', () =>{
 //     navMenu.classList.toggle('mobile-options');
 // });
