c262e6f715d2c1c2750a90333b84dc9f4d506f4b
Luis <luiggiblade@gmail.com>
2021-02-08 22:16:05 -0600

    beta testing

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const navIcon = document.querySelector('.mobile-nav-icon');
 const navMenu = document.querySelector('.nav-pages');
 const topBar = document.querySelector('.top-nav');
 const logo = document.querySelector('.logo');
+const icon = document.querySelector('.menu-icon');
 navIcon.addEventListener('click', () =>{
     navMenu.classList.toggle('mobile-options');
     topBar.classList.toggle('mobile-options');
     logo.classList.toggle('mobile-options');
+    if (icon.classList.contains('.close')){
+        icon.classList.toggle('.close');
+        icon.textContent= 'menu';
+
+    }
+    else{
+        icon.textContent= 'close'
+        icon.classList.toggle('.close');
+    }
 });
 
 // navIcon.addEventListener('touchstart', () =>{
